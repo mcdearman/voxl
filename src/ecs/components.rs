c591cf4884eb75a2e76c3ef5b164ca850ea85c3d
mcdearman <mcdearmanchris@gmail.com>
2025-09-24 12:44:51 -0500

    refactor

--- src/ecs/components.rs
+++
@@ -1,4 +0,0 @@
-pub struct PhysicsComponent;
-pub struct RenderComponent;
-pub struct ChunkPositionComponent;
-pub struct VoxelPositionComponent;
--- src/ecs/entity.rs
+++
@@ -1,6 +0,0 @@
-use crate::ecs::chunk::Chunk;
-
-pub enum Entity {
-    Voxel(u64),
-    Chunk(Chunk),
-}
